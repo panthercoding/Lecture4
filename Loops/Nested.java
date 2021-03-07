64bc70bd85b403eb659cad2c527a2c54de948e4b
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:17:35 -0600

    Create Nested.java

---
+++ Loops/Nested.java
@@ -0,0 +1,21 @@
+
+
+public class Nested{
+  public static void main(String[] args){
+
+    
+    for (int i = 0; i < 5; i++){
+      System.out.println("Hi");
+      for (int j = 0; j < 2; j++){
+        System.out.println("Meow");
+      }
+    }
+    
+    for (int i = 0; i < 5; i++){
+      System.out.println("Hi");
+      System.out.println("Meow");
+      System.out.println("Meow");
+    }
+
+  }
+}
