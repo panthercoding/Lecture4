da285d026b04f5d902fadba0719bee7c07d10dc8
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:12:51 -0600

    Create Chess.java

---
+++ If/Chess.java
@@ -0,0 +1,11 @@
+public class Chess{
+  public static void main(String[] args){
+    int chessElo = 1502;
+    if (chessElo > 2500){
+      System.out.println("Grandmaster");
+    }
+    else{
+      System.out.println("Not a Grandmaster");
+    }
+  }
+  }
