81a119b4fda252c72d01086aa5ac87f7fe4d24ba
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:19:04 -0600

    Create Loops2.java

---
+++ Loops/Loops2.java
@@ -0,0 +1,13 @@
+public class Loops2{
+  public static void main(String[] args){
+
+    for (int i = 0; i < 4; i++){
+      System.out.println("Hello World");
+    }
+
+    for (int j = 99; j > 95; j--){
+      System.out.println(j + " bottles of beer on the wall");
+    }
+
+  }
+}
