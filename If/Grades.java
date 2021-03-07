bd1d51e4dbf749613849e0787103c03d330274b8
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:13:31 -0600

    Create Grades.java

---
+++ If/Grades.java
@@ -0,0 +1,15 @@
+public class Grades{
+  public static void main(String[] args){
+
+    int grade = 85;
+    if (grade >= 90){
+      System.out.println("Grade A");
+    }
+    else if (grade >= 80){
+      System.out.println("Grade B");
+    }
+    else{
+      System.out.println("Grade C or below");
+    }
+}
+}
