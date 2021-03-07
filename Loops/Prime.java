813150887a5680cae920b8f1cad9d81273a6c085
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:18:20 -0600

    Update Prime.java

--- Loops/Prime.java
+++ Loops/Prime.java
@@ -1,6 +1,6 @@
 public class Prime{
   public static void main(String[] args){
-    int p = sumDigits(Integer.parseInt(args[0]));
+    boolean p = isPrime(Integer.parseInt(args[0]));
     System.out.println(p);
   }
 
@@ -16,18 +16,6 @@ public class Prime{
       }
     }
     return(numDivisors==2);
-  }
-
-  public static int sumDigits(int n){
-    
-    int sum = 0; 
-    int digit = 0;
-    while (n>0){ /* while we still have digits */
-      digit = n % 10; //ones digit
-      sum+=digit; //add digit to the sum
-      n /= 10; //removes one digit
-    }  
-    return(sum);
   }
 
 }
