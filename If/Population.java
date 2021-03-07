a4d250ddd7b174ca4e052a25f9bf219514580045
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-03-07 17:12:08 -0600

    Create Population.java

---
+++ If/Population.java
@@ -0,0 +1,21 @@
+public class Population{
+  public static void main(String[] args){
+
+
+    /* populations */
+    int JUNEAU = 32000;
+    int INDIANPOLIS = 864000;
+    int LA = 3967000;
+
+    int TWIN_CITIES = 1200000;
+
+    if (TWIN_CITIES > JUNEAU){
+      System.out.println("The Twin Cities is more populous than Juneau.");
+    }
+    if (TWIN_CITIES > INDIANPOLIS){
+      System.out.println("The Twin Cities is more populous than Indianpolis.");
+    }
+      if (TWIN_CITIES > JUNEAU){
+      System.out.println("The Twin Cities is more populous than Los Angeles.");
+    }
+}
